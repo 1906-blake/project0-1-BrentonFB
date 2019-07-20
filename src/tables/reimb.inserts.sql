23ec3182741b53cc394d8f83cbbac67b8c912665
BrentonByrd <fosterbyrdbrenton@gmail.com>
2019-07-20 13:09:51 -0400

    finished the reimbursement type and status

--- src/tables/reimb.inserts.sql
+++ src/tables/reimb.inserts.sql
@@ -2,3 +2,14 @@ INSERT INTO positions (position)
 VALUES ('admin'), 
 ('finance_manager'), 
 ('employee');
+
+INSERT INTO reimbursementstatus (statusdesc)
+VALUES ('pending'), 
+('approved'),
+('denied');
+
+INSERT INTO reimbursementtype (typeof)
+VALUES ('lodging'),
+('travel'),
+('food'),
+('other');
