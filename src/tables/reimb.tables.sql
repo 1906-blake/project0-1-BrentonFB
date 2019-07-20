99ceb0fa7dbfba89f23bff19b27420c988905f0c
BrentonByrd <fosterbyrdbrenton@gmail.com>
2019-07-20 12:28:54 -0400

    finished the eables

--- src/tables/reimb.tables.sql
+++ src/tables/reimb.tables.sql
@@ -1,14 +1,35 @@
+CREATE TABLE positions (
+    positionid SERIAL PRIMARY KEY,
+    position TEXT NOT NULL UNIQUE
+)
+
 CREATE TABLE users (
-   userId SERIAL PRIMARY KEY,
+   userid SERIAL PRIMARY KEY,
    username TEXT NOT NULL UNIQUE,
    pass TEXT NOT NULL,
    firstname TEXT NOT NULL,
    lastname TEXT NOT NULL,
    email TEXT NOT NULL,
-   position INTEGER NOT NULL 
+   position INTEGER REFERENCES positions(positionid)
 )
 
-CREATE TABLE positions (
-    positionId SERIAL PRIMARY KEY,
-    position TEXT NOT NULL UNIQUE
+CREATE TABLE reimbursementstatus (
+    statusid SERIAL PRIMARY KEY,
+    statusdesc TEXT NOT NULL UNIQUE
+)
+
+CREATE TABLE reimbursementtype (
+    typeid SERIAL PRIMARY KEY,
+    typeof TEXT NOT NULL UNIQUE
+)
+
+CREATE TABLE reimbursement (
+    reimbursementid SERIAL PRIMARY KEY,
+    author INTEGER REFERENCES users(userid),
+    amount NUMERIC (8,2),
+    datesubmitted NUMERIC,
+    dateresolved NUMERIC,
+    resolver INTEGER REFERENCES users(userid),
+    reimbursementstatus INTEGER REFERENCES reimbursementstatus(reimbursementid),
+    reimbursementtype INTEGER REFERENCES reimbursementtype(typeid)
 )
